f2a54ef49988f2ee9bc5b7a33b471ca5b670269a
Azat Satklichov <azat.satklichov@broadcom.com>
2020-07-20 15:32:02 +0200

    trg

---
+++ javatesting/src/main/java/net/sahet/javatesting/tt/Demo.java
@@ -0,0 +1,13 @@
+package net.sahet.javatesting;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("demo ...");
+    }
+
+}
